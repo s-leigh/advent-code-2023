03b579545099277041cfa9351759e4acc514ee14
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-01 10:53:17 +0000

    Add eslint

--- src/day01.ts
+++ src/day01.ts
@@ -1,14 +1,14 @@
-const splitInput = (input: string) => input.split('\n')
+const splitInput = (input: string) => input.split("\n")
 
 export const day01Part01 = (input: string): number => {
-    const numbers = splitInput(input)
-        .map(line => line.split('')
-            .filter(char => /[0-9]/.test(char))
-            .filter((_, i, arr) => i === 0 || i === arr.length - 1)
-        )
+  const numbers = splitInput(input)
+    .map(line => line.split("")
+      .filter(char => /[0-9]/.test(char))
+      .filter((_, i, arr) => i === 0 || i === arr.length - 1)
+    )
 
-    const combined = numbers.map(line => line.length === 1 ? line.concat([line[0]]) : line)
-        .map(line => line.reduce((prev, curr) => prev + curr))
+  const combined = numbers.map(line => line.length === 1 ? line.concat([line[0]]) : line)
+    .map(line => line.reduce((prev, curr) => prev + curr))
 
-    return combined.map(strNum => parseInt(strNum)).reduce((prev, curr) => prev + curr)
+  return combined.map(strNum => parseInt(strNum)).reduce((prev, curr) => prev + curr)
 }
--- test/day01.test.ts
+++ test/day01.test.ts
@@ -1,21 +1,21 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
 import { day01Part01 } from "../src/day01"
-import fs from 'fs'
+import fs from "fs"
 
 describe("Day 01", () => {
-    test("Day 01 part 01 sample input ", () => {
-        const sampleInput = `1abc2
+  test("Day 01 part 01 sample input ", () => {
+    const sampleInput = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
-        const result = day01Part01(sampleInput)
-        assert.strictEqual(result, 142)
-    })
+    const result = day01Part01(sampleInput)
+    assert.strictEqual(result, 142)
+  })
 
-    test("Day 01 part 01", () => {
-        const input = fs.readFileSync('./input/day01Input').toString()
-        const result = day01Part01(input)
-        assert.strictEqual(result, 54388)
-    })
+  test("Day 01 part 01", () => {
+    const input = fs.readFileSync("./input/day01Input").toString()
+    const result = day01Part01(input)
+    assert.strictEqual(result, 54388)
+  })
 })
