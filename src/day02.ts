597df44bfc1c228d0c8f1cdc07fd0779d21af8db
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-02 09:45:18 +0000

    Day 02

--- src/day02.ts
+++ src/day02.ts
@@ -6,24 +6,41 @@ type GamePart = {
   blue: number
 }
 
-// What is possible with only 12 red cubes, 13 green cubes, and 14 blue cubes?
-const isGamePartPossible = (gamePart: GamePart): boolean =>
-  gamePart.red <= 12 && gamePart.green <= 13 && gamePart.blue <= 14
+const getGames = (inputLines: string[]): string[][] => inputLines.map(line => line.split(":")[1].split(";"))
+
+const getGamesWithGameParts = (games: string[][]): GamePart[][] => games.map(game => game.map(gamePart => gamePart.split(",").reduce((acc, curr) => {
+  const number = parseInt(curr.split(" ")[1])
+  if (curr.includes("red")) acc.red += number
+  else if (curr.includes("green")) acc.green += number
+  else if (curr.includes("blue")) acc.blue += number
+  return acc
+}, { red: 0, green: 0, blue: 0 } as GamePart)))
 
 export const day02Part01 = (input: string): number => {
-  const inputLines = splitInputIntoLines(input)
-  const games = inputLines.map(line => line.split(":")[1].split(";"))
-  
-  const gamesWithGameParts = games.map(game => game.map(gamePart => gamePart.split(",").reduce((acc, curr) => {
-    const number = parseInt(curr.split(" ")[1])
-    if (curr.includes("red")) acc.red += number
-    else if (curr.includes("green")) acc.green += number
-    else if (curr.includes("blue")) acc.blue += number
-    return acc
-  }, {red: 0, green: 0, blue: 0} as GamePart)))
+  const games = getGames(splitInputIntoLines(input))
+  const gamesWithGameParts = getGamesWithGameParts(games)
+
+  // What is possible with only 12 red cubes, 13 green cubes, and 14 blue cubes?
+  const isGamePartPossible = (gamePart: GamePart): boolean =>
+    gamePart.red <= 12 && gamePart.green <= 13 && gamePart.blue <= 14
 
   const possibleGamePartsValues = gamesWithGameParts.map(game => game.map(isGamePartPossible))
     .map((gamePartResults, i) => gamePartResults.includes(false) ? 0 : i + 1)
 
   return possibleGamePartsValues.reduce((prev, curr) => prev + curr)
+}
+
+export const day02Part02 = (input: string): number => {
+  const games = getGames(splitInputIntoLines(input))
+  const gamesWithGameParts = getGamesWithGameParts(games)
+
+  const maximums = gamesWithGameParts.map(game => game.reduce((acc, curr) => {
+    if (curr.red > acc.red) acc.red = curr.red
+    if (curr.green > acc.green) acc.green = curr.green
+    if (curr.blue > acc.blue) acc.blue = curr.blue
+    return acc
+  }, { red: 0, green: 0, blue: 0 }))
+
+  return maximums.map(maxCubes => Object.values(maxCubes).reduce((acc, curr) => acc * curr))
+    .reduce((acc, curr) => acc + curr)
 }
--- test/day02.test.ts
+++ test/day02.test.ts
@@ -1,6 +1,6 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
-import { day02Part01 } from "../src/day02"
+import { day02Part01, day02Part02 } from "../src/day02"
 import fs from "fs"
 
 describe("Day 02", () => {
@@ -14,9 +14,25 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
     assert.strictEqual(result, 8)
   })
 
-  test("Day 02 part 1", () => {
+  test("Day 02 part 01", () => {
     const input = fs.readFileSync("./input/day02Input").toString()
     const result = day02Part01(input)
     assert.strictEqual(result, 2600)
+  })
+
+  test("Day 02 part 02 sample input ", () => {
+    const sampleInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+    const result = day02Part02(sampleInput)
+    assert.strictEqual(result, 2286)
+  })
+
+  test("Day 02 part 02", () => {
+    const input = fs.readFileSync("./input/day02Input").toString()
+    const result = day02Part02(input)
+    assert.strictEqual(result, 86036)
   })
 })
