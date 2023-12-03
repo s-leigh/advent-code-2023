b57f2d71b92c3bfd8fd6a17170082a99f21d0c22
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-03 13:33:41 +0000

    Day 03 improved

--- src/day03.ts
+++ src/day03.ts
@@ -1,6 +1,7 @@
 import { splitInputIntoLines } from "./common"
 
-const PARSING_REGEX = /(?<number>[0-9]+)|(?<symbol>[^.|\r])/g
+const PART_1_REGEX = /(?<number>[0-9]+)|(?<symbol>[^.|\r])/g
+const PART_2_REGEX = /(?<number>[0-9]+)|(?<symbol>\*)/g
 
 type Element = {
   x: number,
@@ -10,51 +11,55 @@ type Element = {
   type: "number" | "symbol"
 }
 
-const parseLine = (inputLine: string, yIndex: number): Element[] => {
-  const matches = [...inputLine.matchAll(PARSING_REGEX)]
-  return matches.map(match => ({
-    x: match.index!,
-    y: yIndex,
-    length: match.groups?.symbol ? 1 : match[0].length,
-    contents: match[0],
-    type: match.groups?.symbol ? "symbol" : "number"
-  }))
+const parseInput = (
+  splitInput: string[],
+  regex: RegExp,
+  symbols: Element[] = [],
+  numbers: Element[] = [],
+  yIndex: number = 0
+): [Element[], Element[]] => {
+  if (splitInput.length === 0) return [symbols, numbers]
+
+  const newSymbols: Element[] = []
+  const newNumbers: Element[] = []
+  for (const match of splitInput[0].matchAll(regex)) {
+    const type: "symbol" | "number" = match.groups?.symbol ? "symbol" : "number"
+    const element = {
+      x: match.index!,
+      y: yIndex,
+      length: match.groups?.symbol ? 1 : match[0].length,
+      contents: match[0],
+      type
+    }
+    element.type === "symbol" ? newSymbols.push(element) : newNumbers.push(element)
+  }
+
+  return parseInput(splitInput.slice(1), regex, symbols.concat(newSymbols), numbers.concat(newNumbers), yIndex + 1)
 }
 
 const isNearSymbol = (number: Element, symbol: Element): boolean => {
-  const validXValues = Array(number.length).fill("").map((_, i) => number.x + i)
-  const isValidX = number.x === symbol.x || validXValues.map(vx => Math.abs(vx - symbol.x)).includes(1)
-  return Math.abs(symbol.y - number.y) <= 1 && isValidX
+  const isValidY = Math.abs(symbol.y - number.y) <= 1
+  if (!isValidY) return false
+  return Math.abs(number.x - symbol.x) <= 1 || (number.x < symbol.x && symbol.x - number.x <= number.contents.length)
 }
 
 export const day03Part01 = (input: string): number => {
-  const elements = splitInputIntoLines(input).map((inputLine, i) => parseLine(inputLine, i))
-
-  const symbols = elements.flatMap(line => line.filter(e => e.type === "symbol"))
-  const numbers = elements.flatMap(line => line.filter(e => e.type === "number"))
-  const partNumbers: Element[] = []
-  symbols.forEach(s => numbers.forEach(n => { if (isNearSymbol(n, s)) partNumbers.push(n) }))
-
+  const [symbols, numbers] = parseInput(splitInputIntoLines(input), PART_1_REGEX)
+  const partNumbers = symbols.flatMap(s => numbers.filter(n => isNearSymbol(n, s)))
   return partNumbers.reduce((prev, curr) => prev + parseInt(curr.contents), 0)
 }
 
 export const day03Part02 = (input: string): number => {
-  const elements = splitInputIntoLines(input).map((inputLine, i) => parseLine(inputLine, i))
+  const [asterisks, numbers] = parseInput(splitInputIntoLines(input), PART_2_REGEX)
 
-  const asterisks = elements.flatMap(line => line.filter(e => e.type === "symbol" && e.contents === "*"))
-  const numbers = elements.flatMap(line => line.filter(e => e.type === "number"))
-  // asterisk coords are x,y as string, e.g. (3,4) is "3,4", 0-indexed
-  const nearAsterisk: { [asteriskCoords: string]: Element[] } = {}
-  asterisks.forEach(s => numbers.forEach(n => {
-    if (isNearSymbol(n, s)) {
-      const asteriskCoordsKey = `${s.x},${s.y}`
-      nearAsterisk[asteriskCoordsKey] ? nearAsterisk[asteriskCoordsKey].push(n) : nearAsterisk[asteriskCoordsKey] = [n]
+  const nearAsteriskIndexed: Element[][] = []
+  asterisks.forEach((asterisk, asteriskI) => numbers.forEach(n => {
+    if (isNearSymbol(n, asterisk)) {
+      nearAsteriskIndexed[asteriskI] ? nearAsteriskIndexed[asteriskI].push(n) : nearAsteriskIndexed[asteriskI] = [n]
     }
   }))
 
-  const gears = Object.keys(nearAsterisk)
-    .filter(asteriskCoords => nearAsterisk[asteriskCoords].length == 2).map(asteriskCoords => nearAsterisk[asteriskCoords])
-  
+  const gears = nearAsteriskIndexed.filter(elements => elements.length == 2)
   return gears
     .map(e => parseInt(e[0].contents) * parseInt(e[1].contents))
     .reduce((a, b) => a + b)
