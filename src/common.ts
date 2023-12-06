e94f18ad95aa2fa6e7073ca96c3173346d8a7415
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-06 14:19:08 +0000

    Day 06 improved

--- src/common.ts
+++ src/common.ts
@@ -15,3 +15,5 @@ Array.prototype.min = function () { return this.reduce((a, b) => b < a ? b : a) 
 Array.prototype.product = function () { return this.reduce((a, b) => a * b) }
 
 export const splitInputIntoLines = (input: string) => input.split("\n")
+
+export const arrayOfIndices = (length: number) => Array(length).fill(0).map((_, i) => i)
--- src/day06.ts
+++ src/day06.ts
@@ -1,4 +1,4 @@
-import { splitInputIntoLines } from "./common"
+import { arrayOfIndices, splitInputIntoLines } from "./common"
 
 const distance = (holdTime: number, raceTime: number): number => (raceTime - holdTime) * holdTime
 
@@ -6,37 +6,20 @@ export const day06Part01 = (input: string): number => {
   const [times, winningDistances] = splitInputIntoLines(input).map(l =>
     l.split(/:|\s+/g).filterNotEmpty().map(s => parseInt(s)).filter(x => !isNaN(x))
   )
-  const winnerCounts: number[] = Array(times.length).fill(0)
-  times.forEach((t, raceI) => {
-    for (let i = 0; i < t; i++) {
-      const d = distance(i, t)
-      if (d > winningDistances[raceI]) winnerCounts[raceI]++
-    }
-  })
-  return winnerCounts.product()
+
+  return times.map((t, raceI) =>
+    arrayOfIndices(t)
+      .filter(i => distance(i, t) > winningDistances[raceI])
+      .length
+  ).product()
 }
 
 export const day06Part02 = (input: string): number => {
-  const splitInput = splitInputIntoLines(input).map(l =>
-    l.split(/:/g).filterNotEmpty()
-  )
+  const splitInput = splitInputIntoLines(input).map(l => l.split(/:/g).filterNotEmpty())
   const [raceTime, winningDistance] = [splitInput[0][1], splitInput[1][1]].map(segment => segment.split(" ").filterNotEmpty().join("")).map(s => parseInt(s))
 
-  let lowestWin = undefined
-  let highestWin = undefined
-  for (let i = 0; i <= raceTime; i++) {
-    const possibleHighest = distance(raceTime - i, raceTime)
-    const possibleLowest = distance(i, raceTime)
-    if (possibleHighest > winningDistance) {
-      highestWin = raceTime - i + 1
-      if (lowestWin) break
-    }
-    if (possibleLowest > winningDistance) {
-      lowestWin = i
-      if (highestWin) break
-    }
-  }
-
-  if (!highestWin || !lowestWin) throw new Error("Could not find highest and/or lowest")
+  const squareRootOfBSquaredMinusFourAC = Math.sqrt(raceTime ** 2 - 4 * winningDistance)
+  const highestWin = Math.floor((raceTime + squareRootOfBSquaredMinusFourAC) / 2)
+  const lowestWin = Math.floor((raceTime - squareRootOfBSquaredMinusFourAC) / 2)
   return highestWin - lowestWin
 }
