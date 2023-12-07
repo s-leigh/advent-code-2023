e28c8f3cac3ea10d1228b225a38f7f86069349d2
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-07 16:30:20 +0000

    Day 07

--- src/day07.ts
+++ src/day07.ts
@@ -1,36 +1,8 @@
 import { splitInputIntoLines } from "./common"
 
-const handStrength = (hand: string, jokers: boolean = false): 1 | 2 | 3 | 4 | 5 | 6 | 7 => {
-  const handAsMap: Map<string, number> = new Map()
-  hand.split("").forEach(c => handAsMap.set(c, handAsMap.get(c) ? handAsMap.get(c)! + 1 : 1))
-
-  if (!jokers || ![...handAsMap.keys()].includes("J")) {
-    const cardValues = [...handAsMap.values()]
-    if (cardValues.includes(5)) return 7 // 5 of a kind
-    if (cardValues.includes(4)) return 6 // 4 of a kind
-    if (cardValues.includes(3) && cardValues.includes(2)) return 5 // full house
-    if (cardValues.includes(3)) return 4 // 3 of a kind
-    if (cardValues.filter(x => x === 2).length === 2) return 3 // two pair
-    if (cardValues.includes(2)) return 2 // one pair
-    return 1 // high card
-  }
-
-  let mostCommonCard: string | undefined = undefined
-  handAsMap.forEach((val, key) => {
-    if (!mostCommonCard) mostCommonCard = key
-    else if (val > handAsMap.get(mostCommonCard)!) mostCommonCard = key
-  })
-  // If all cards are different, replace with highest-value card
-  if (handAsMap.get(mostCommonCard!) === 1) {
-    const replacementValue = hand.split("").map(c => individualCardVal(c, true)).max()
-    mostCommonCard = replacementValue < 10 && replacementValue >= 2 ? replacementValue.toString() : {
-      10: "T",
-      12: "Q",
-      13: "K",
-      14: "A"
-    }[replacementValue]
-  }
-  return handStrength(hand.replace(/J/g, mostCommonCard!))
+const parseInput = (input: string): [string[], number[]] =>  {
+  const splitInput = splitInputIntoLines(input).map(line => line.split(" "))
+  return [splitInput.map(part => part[0]), splitInput.map(part => part[1]).map(x => parseInt(x))]
 }
 
 const individualCardVal = (card: string, jokers: boolean = false): number => {
@@ -39,8 +11,43 @@ const individualCardVal = (card: string, jokers: boolean = false): number => {
   return vals[card as keyof typeof vals]
 }
 
-const sortedHands = (hands: string[], jokers: boolean = false): number[][] => {
-  const handStrengthsAndIndices = hands.map((h, i) => [handStrength(h, jokers), i])
+const replaceJokers = (hand: string): string => {
+  const handAsMap: Map<string, number> = new Map()
+  hand.split("").filter(c => c !== "J").forEach(c => handAsMap.set(c, handAsMap.get(c) ? handAsMap.get(c)! + 1 : 1))
+
+  let replacementCard: string | undefined = undefined
+  handAsMap.forEach((val, key) => {
+    if (!replacementCard || val > handAsMap.get(replacementCard)!) replacementCard = key
+  })
+  // If all cards are different, replace with highest-value card
+  if (handAsMap.get(replacementCard!) === 1) {
+    const replacementValue = hand.split("").map(c => individualCardVal(c, true)).max()
+    replacementCard = replacementValue < 10 && replacementValue >= 2 ? replacementValue.toString() : {
+      10: "T",
+      12: "Q",
+      13: "K",
+      14: "A"
+    }[replacementValue]
+  }
+  return hand.replace(/J/g, replacementCard!)
+}
+
+const handStrength = (hand: string): 1 | 2 | 3 | 4 | 5 | 6 | 7 => {
+  const handAsMap: Map<string, number> = new Map()
+  hand.split("").forEach(c => handAsMap.set(c, handAsMap.get(c) ? handAsMap.get(c)! + 1 : 1))
+
+  const cardValues = [...handAsMap.values()]
+  if (cardValues.includes(5)) return 7 // 5 of a kind
+  if (cardValues.includes(4)) return 6 // 4 of a kind
+  if (cardValues.includes(3) && cardValues.includes(2)) return 5 // full house
+  if (cardValues.includes(3)) return 4 // 3 of a kind
+  if (cardValues.filter(x => x === 2).length === 2) return 3 // two pair
+  if (cardValues.includes(2)) return 2 // one pair
+  return 1 // high card
+}
+
+export const sortedHands = (hands: string[], jokers: boolean = false): number[][] => {
+  const handStrengthsAndIndices = hands.map(h => jokers ? replaceJokers(h) : h).map((h, i) => [handStrength(h), i])
   const sameHandStrengthSort = (a: string, b: string, i: number = 0): number => {
     const [aVal, bVal] = [individualCardVal(a[0], jokers), individualCardVal(b[0], jokers)]
     if (aVal === bVal) return sameHandStrengthSort(a.slice(1), b.slice(1), i + 1)
@@ -54,15 +61,13 @@ const sortedHands = (hands: string[], jokers: boolean = false): number[][] => {
 }
 
 export const day07Part01 = (input: string): number => {
-  const splitInput = splitInputIntoLines(input).map(line => line.split(" "))
-  const [hands, bids] = [splitInput.map(part => part[0]), splitInput.map(part => part[1]).map(x => parseInt(x))]
+  const [hands, bids] = parseInput(input)
   const sorted = sortedHands(hands)
   return sorted.map((strengthAndOriginalI, i) => (i + 1) * bids[strengthAndOriginalI[1]]).sum()
 }
 
 export const day07Part02 = (input: string): number => {
-  const splitInput = splitInputIntoLines(input).map(line => line.split(" "))
-  const [hands, bids] = [splitInput.map(part => part[0]), splitInput.map(part => part[1]).map(x => parseInt(x))]
+  const [hands, bids] = parseInput(input)
   const sorted = sortedHands(hands, true)
   return sorted.map((strengthAndOriginalI, i) => (i + 1) * bids[strengthAndOriginalI[1]]).sum()
 }
--- test/day07.test.ts
+++ test/day07.test.ts
@@ -9,12 +9,37 @@ KK677 28
 KTJJT 220
 QQQJA 483`
 
+const SAMPLE_INPUT_2 = `2345A 1
+Q2KJJ 13
+Q2Q2Q 19
+T3T3J 17
+T3Q33 11
+2345J 3
+J345A 2
+32T3K 5
+T55J5 29
+KK677 7
+KTJJT 34
+QQQJA 31
+JJJJJ 37
+JAAAA 43
+AAAAJ 59
+AAAAA 61
+2AAAA 23
+2JJJJ 53
+JJJJ2 41`
+
 const input = fs.readFileSync("./input/day07Input").toString()
 
 describe("Day 07", () => {
   test("Day 07 part 01 sample input", () => {
     const result = day07Part01(SAMPLE_INPUT)
     assert.strictEqual(result, 6440)
+  })
+
+  test("Day 07 part 01 sample input 2", () => {
+    const result = day07Part01(SAMPLE_INPUT_2)
+    assert.strictEqual(result, 6592)
   })
 
   test("Day 07 part 01", () => {
@@ -25,5 +50,14 @@ describe("Day 07", () => {
   test("Day 07 part 02 sample input", () => {
     const result = day07Part02(SAMPLE_INPUT)
     assert.strictEqual(result, 5905)
+  })
+
+  test("Day 07 part 02 sample input 2", () => {
+    const result = day07Part02(SAMPLE_INPUT_2)
+    assert.strictEqual(result, 6839)  })
+
+  test("Day 07 part 02", () => {
+    const result = day07Part02(input)
+    assert.strictEqual(result, 253907829)
   })
 })
