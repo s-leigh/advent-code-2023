0e0ee44a918529647477c294d1e2906df9546fac
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-08 12:25:35 +0000

    Day 08

--- src/day08.ts
+++ src/day08.ts
@@ -1,31 +1,74 @@
 import { splitInputIntoLines } from "./common"
 
+type Direction = "L" | "R"
+
 type Node = {
-  L: string
-  R: string
+  [K in Direction]: string
 }
 
-const inputLineAsNode = (inputLine: string): Node => {
-  const [l, r] = [inputLine.split(",")[0].slice(-3), inputLine.split(",")[1].slice(1,4)]
-  return {L: l, R: r}
+const parseInput = (input: string): [Direction[], Map<string, Node>] => {
+  const inputLineAsNode = (inputLine: string): Node => {
+    const [L, R] = [inputLine.split(",")[0].slice(-3), inputLine.split(",")[1].slice(1, 4)]
+    return { L, R }
+  }
+  const inputAsLines = splitInputIntoLines(input)
+  const directions = inputAsLines[0].split("").filter(x => x !== "\r") as Direction[]
+  const nodes = inputAsLines.slice(2).reduce((acc, l) => {
+    acc.set(l.slice(0, 3), inputLineAsNode(l))
+    return acc
+  }, new Map<string, Node>())
+  return [directions, nodes]
 }
 
 export const day08Part01 = (input: string): number => {
-  const inputAsLines = splitInputIntoLines(input)
-  const directions = inputAsLines[0].split("").filter(x => x !== "\r")
-  const nodes = inputAsLines.slice(2).reduce((acc, l) => {
-    acc.set(l.slice(0,3), inputLineAsNode(l))
-    return acc
-  }, new Map<string, Node>())
+  const [directions, nodes] = parseInput(input)
 
   let numberOfSteps = 0
   let currentNode: Node = nodes.get("AAA")!
   while (currentNode !== nodes.get("ZZZ")) {
     directions.forEach(dir => {
-      const nextNode = nodes.get(currentNode[dir as keyof Node])!
+      const nextNode = nodes.get(currentNode[dir])!
       currentNode = nextNode
       numberOfSteps++
     })
   }
   return numberOfSteps
+}
+
+export const day08Part02 = (input: string): number => {
+  const [directions, nodes] = parseInput(input)
+
+  const lastLetter = (s: string) => s[s.length - 1]
+
+  const currentNodes: Map<string, Node> = [...nodes.keys()]
+    .filter(k => lastLetter(k) === "A")
+    .reduce((acc, k) => {
+      acc.set(k, nodes.get(k)!)
+      return acc
+    }, new Map<string, Node>())
+
+  let numberOfSteps = 0
+  const loopSizes: number[] = []
+  while (currentNodes.size > 0) {
+    directions.forEach(dir => {
+      [...currentNodes.keys()].forEach(currentKey => {
+        const newKey = nodes.get(currentKey)![dir]
+        if (lastLetter(newKey) === "Z") {
+          loopSizes.push(numberOfSteps + 1)
+        } else {
+          currentNodes.set(newKey, nodes.get(newKey)!)
+        }
+        currentNodes.delete(currentKey)
+      })
+      numberOfSteps++
+    })
+  }
+
+  const greatestCommonDenominator = (a: number, b: number) => {
+    while (b > 0) [a, b] = [b, a % b]
+    return a
+  }
+  const lowestCommonMultiple = (a: number, b: number) => (a * b) / greatestCommonDenominator(a, b)
+
+  return loopSizes.reduce((acc, curr) => lowestCommonMultiple(curr, acc), 1)
 }
--- test/day08.test.ts
+++ test/day08.test.ts
@@ -1,9 +1,9 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
 import fs from "fs"
-import { day08Part01 } from "../src/day08"
+import { day08Part01, day08Part02 } from "../src/day08"
 
-const SAMPLE_INPUT_1 = `RL
+const PART_1_SAMPLE_INPUT_1 = `RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -13,27 +13,48 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)`
 
-const SAMPLE_INPUT_2 = `LLR
+const PART_2_SAMPLE_INPUT_2 = `LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`
 
+const PART_2_SAMPLE_INPUT = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+
 const input = fs.readFileSync("./input/day08Input").toString()
 
 describe("Day 08", () => {
   test("Day 08 part 01 sample input 01", () => {
-    const result = day08Part01(SAMPLE_INPUT_1)
+    const result = day08Part01(PART_1_SAMPLE_INPUT_1)
     assert.strictEqual(result, 2)
   })
 
   test("Day 08 part 01 sample input 02", () => {
-    const result = day08Part01(SAMPLE_INPUT_2)
+    const result = day08Part01(PART_2_SAMPLE_INPUT_2)
     assert.strictEqual(result, 6)
   })
 
   test("Day 08 part 01", () => {
     const result = day08Part01(input)
     assert.strictEqual(result, 20513)
+  })
+
+  test("Day 08 part 02 sample input 01", () => {
+    const result = day08Part02(PART_2_SAMPLE_INPUT)
+    assert.strictEqual(result, 6)
+  })
+
+  test("Day 08 part 02", () => {
+    const result = day08Part02(input)
+    assert.strictEqual(result, 15995167053923)
   })
 })
