931e7978fa36418ef0b74fa53f276f9d10254cd0
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-09 09:22:38 +0000

    Day 09

--- src/day09.ts
+++ src/day09.ts
@@ -11,9 +11,20 @@ const getDeltas = (input: number[], result: number[][] = []): number[][] => {
   return getDeltas(deltas, result.concat([[...deltas]]))
 }
 
+const parseInput = (input: string): number[][] =>
+  splitInputIntoLines(input).map(l => l.split(" ").map(s => parseInt(s)))
+
+const getLastDeltasSum = (line: number[]): number => getDeltas(line)
+  .map(deltasForLine => deltasForLine.last()).sum()
+
 export const day09Part01 = (input: string): number => {
-  const lines = splitInputIntoLines(input).map(l => l.split(" ").map(s => parseInt(s)))
-  const deltas = lines.map(l => getDeltas(l))
-  const deltaSums = deltas.map(deltasForLine => deltasForLine.map(d => d.last()).sum())
+  const lines = parseInput(input)
+  const deltaSums = lines.map(l => getLastDeltasSum(l))
+  return lines.map(l => l.last()).map((x, i) => x + deltaSums[i]).sum()
+}
+
+export const day09Part02 = (input: string): number => {
+  const lines = parseInput(input).map(arr => arr.reverse())
+  const deltaSums = lines.map(l => getLastDeltasSum(l))
   return lines.map(l => l.last()).map((x, i) => x + deltaSums[i]).sum()
 }
--- test/day09.test.ts
+++ test/day09.test.ts
@@ -1,7 +1,7 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
 import fs from "fs"
-import { day09Part01 } from "../src/day09"
+import { day09Part01, day09Part02 } from "../src/day09"
 
 const SAMPLE_INPUT = `0 3 6 9 12 15
 1 3 6 10 15 21
@@ -18,5 +18,15 @@ describe("Day 09", () => {
   test("Day 09 part 01 ", () => {
     const result = day09Part01(input)
     assert.strictEqual(result, 1901217887)
+  })
+
+  test("Day 09 part 02 sample input", () => {
+    const result = day09Part02(SAMPLE_INPUT)
+    assert.strictEqual(result, 2)
+  })
+
+  test("Day 09 part 02", () => {
+    const result = day09Part02(input)
+    assert.strictEqual(result, 905)
   })
 })
