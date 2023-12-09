3933e7a17a434c268f0939429d9ffc0b58782df6
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-09 12:15:50 +0000

    Day 09 improved

--- src/day09.ts
+++ src/day09.ts
@@ -1,30 +1,27 @@
 import { splitInputIntoLines } from "./common"
 
-const getDeltas = (input: number[], result: number[][] = []): number[][] => {
-  if (input.filter(x => x !== 0).length === 0) return result
+const lastDeltasSum = (input: number[], result: number = 0): number => {
+  if (input.every(x => x === 0)) return result
   const deltas: number[] = input.reduce((prev, curr, currI, arr) => {
     if (currI > 0) {
       prev.push(curr - arr[currI - 1])
     }
     return prev
   }, [] as number[])
-  return getDeltas(deltas, result.concat([[...deltas]]))
+  return lastDeltasSum(deltas, result + deltas.last())
 }
 
 const parseInput = (input: string): number[][] =>
   splitInputIntoLines(input).map(l => l.split(" ").map(s => parseInt(s)))
 
-const getLastDeltasSum = (line: number[]): number => getDeltas(line)
-  .map(deltasForLine => deltasForLine.last()).sum()
-
 export const day09Part01 = (input: string): number => {
   const lines = parseInput(input)
-  const deltaSums = lines.map(l => getLastDeltasSum(l))
-  return lines.map(l => l.last()).map((x, i) => x + deltaSums[i]).sum()
+  const deltaSums = lines.map(l => lastDeltasSum(l))
+  return lines.reduce((prev, currL, i) => prev + currL.last() + deltaSums[i], 0)
 }
 
 export const day09Part02 = (input: string): number => {
   const lines = parseInput(input).map(arr => arr.reverse())
-  const deltaSums = lines.map(l => getLastDeltasSum(l))
-  return lines.map(l => l.last()).map((x, i) => x + deltaSums[i]).sum()
+  const deltaSums = lines.map(l => lastDeltasSum(l))
+  return lines.reduce((prev, currL, i) => prev + currL.last() + deltaSums[i], 0)
 }
