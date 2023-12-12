b38da226b3a0a0cb0abb1e39bab2308cc84c928d
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-12 10:36:05 +0000

    Day 11

--- src/day11.ts
+++ src/day11.ts
@@ -1,6 +1,6 @@
 import { splitInputIntoLines } from "./common"
 
-const expandUniverse = (universe: string[][]) => {
+const getEmptyLines = (universe: string[][]): [number[], number[]] => {
   const emptyColumnFieldCount: number[] = Array(universe[0].length).fill(0)
   const emptyRowIndices: number[] = []
   universe.forEach((row, y) => {
@@ -10,6 +10,11 @@ const expandUniverse = (universe: string[][]) => {
     })
   })
   const emptyColumnIndices = emptyColumnFieldCount.reduce((acc, curr, i) => curr === universe.length ? acc.concat([i]) : acc, [] as number[])
+  return [emptyColumnIndices, emptyRowIndices]
+}
+
+const expandUniverse = (universe: string[][]) => {
+  const [emptyColumnIndices, emptyRowIndices] = getEmptyLines(universe)
   emptyColumnIndices.forEach((eci, delta) => { // index increases each time we add a line
     universe.forEach(row => row.splice(eci + delta, 0, "."))
   })
@@ -21,17 +26,35 @@ const expandUniverse = (universe: string[][]) => {
 
 const uniquePairs = (arr: number[][]) => arr.flatMap((coOrds1, i) => arr.slice(i + 1).map(coOrds2 => [coOrds1, coOrds2]))
 
-export const day11Part01 = (input: string): number => {
-  const universe = splitInputIntoLines(input).map(line => line.split(""))
-  expandUniverse(universe)
-
+const getGalaxyCoords = (universe: string[][]): number[][] => {
   const galaxyCoords: number[][] = []
   universe.forEach((row, y) => {
     row.forEach((elem, x) => {
       if (elem === "#") galaxyCoords.push([x, y])
     })
   })
+  return galaxyCoords
+}
 
+export const day11Part01 = (input: string): number => {
+  const universe = splitInputIntoLines(input).map(line => line.split(""))
+  expandUniverse(universe)
+
+  const galaxyCoords = getGalaxyCoords(universe)
   const pathLengths = uniquePairs(galaxyCoords).map(([[x1, y1], [x2, y2]]) => Math.abs(x1 - x2) + Math.abs(y1 - y2))
+  return pathLengths.sum()
+}
+
+export const day11Part02 = (input: string, factor: number): number => {
+  const universe = splitInputIntoLines(input).map(line => line.split(""))
+  const emptyLines = getEmptyLines(universe)
+  const galaxyCoords = getGalaxyCoords(universe)
+  const pathLengths = uniquePairs(galaxyCoords).map(([[x1, y1], [x2, y2]]) => {
+    const [lowerX, higherX] = [x1 < x2 ? x1 : x2, x1 < x2 ? x2 : x1]
+    const [lowerY, higherY] = [y1 < y2 ? y1 : y2, y1 < y2 ? y2 : y1]
+    const numberOfEmptyLinesCrossed = emptyLines[0].filter(x => x > lowerX && x < higherX).length + emptyLines[1].filter(y => y > lowerY && y < higherY).length
+    return higherX - lowerX + higherY - lowerY + (numberOfEmptyLinesCrossed * factor) - numberOfEmptyLinesCrossed
+  })
+
   return pathLengths.sum()
 }
--- test/day11.test.ts
+++ test/day11.test.ts
@@ -1,7 +1,7 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
 import fs from "fs"
-import { day11Part01 } from "../src/day11"
+import { day11Part01, day11Part02 } from "../src/day11"
 
 const SAMPLE_INPUT = `...#......
 .......#..
@@ -25,5 +25,20 @@ describe("Day 11", () => {
   test("Day 11 part 01", () => {
     const result = day11Part01(input)
     assert.strictEqual(result, 10422930)
+  })
+
+  test("Day 11 part 02 sample input 1", () => {
+    const result = day11Part02(SAMPLE_INPUT, 10)
+    assert.strictEqual(result, 1030)
+  })
+
+  test("Day 11 part 02 sample input 2", () => {
+    const result = day11Part02(SAMPLE_INPUT, 100)
+    assert.strictEqual(result, 8410)
+  })
+
+  test("Day 11 part 02", () => {
+    const result = day11Part02(input, 1000000)
+    assert.strictEqual(result, 699909023130)
   })
 })
