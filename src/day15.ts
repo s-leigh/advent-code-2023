717994b5160497b6d7fcada18af01d736c3a1187
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2023-12-15 09:45:04 +0000

    Day 15

--- src/day15.ts
+++ src/day15.ts
@@ -1,4 +1,14 @@
+type Lens = {
+  label: string
+  focalLength: number
+}
 
+type Instruction = {
+  label: string
+  boxNumber: number
+  instruction: "-" | "="
+  focalLength: number
+}
 
 const hash = (input: string): number => {
   const ascii = input.split("").map(char => char.charCodeAt(0))
@@ -11,6 +21,38 @@ const hash = (input: string): number => {
   return val
 }
 
-export const day15Part01 = (input: string): number => {
-  return input.split(",").map(substr => hash(substr)).reduce((a, b) => a + b, 0)
+const lensFocusingPower = (boxNumber: number, slotNumber: number, focalLength: number): number => (boxNumber + 1) * slotNumber * focalLength
+
+const boxFocusingPower = (lenses: Lens[], boxNo: number) => lenses.reduce((a, b, i) => a + lensFocusingPower(boxNo, i + 1, b.focalLength), 0)
+
+export const day15Part01 = (input: string): number => input.split(",").reduce((a, b) => a + hash(b), 0)
+
+export const day15Part02 = (input: string): number => {
+  const instructions: Instruction[] = input.split(",").map(input => {
+    const instruction = input.match(/-|=/)![0] as "-" | "="
+    const [label, focalLength] = input.split(instruction)
+    const boxNumber = hash(label)
+    return { label, boxNumber, instruction, focalLength: instruction === "-" ? -1 : parseInt(focalLength) }
+  })
+
+  const boxes: Map<number, Lens[]> = new Map()
+  instructions.forEach(({ label, boxNumber, instruction, focalLength }) => {
+    const lenses = boxes.get(boxNumber)
+    if (instruction === "=") {
+      const lensToAdd: Lens = { label, focalLength }
+      if (!lenses) {
+        boxes.set(boxNumber, [lensToAdd])
+      } else {
+        const existingLensIndex = lenses.map(l => l.label).indexOf(label)
+        existingLensIndex === -1 ? lenses.push(lensToAdd) : lenses[existingLensIndex] = lensToAdd
+      }
+    } else {
+      if (!lenses) return
+      const existingLensIndex = lenses.map(l => l.label).indexOf(label)
+      if (existingLensIndex === -1) return
+      lenses.splice(existingLensIndex, 1)
+    }
+  })
+
+  return [...boxes.keys()].reduce((a, b) => a + boxFocusingPower(boxes.get(b)!, b), 0)
 }
--- test/day15.test.ts
+++ test/day15.test.ts
@@ -1,7 +1,7 @@
 import { describe, test } from "node:test"
 import assert from "node:assert"
 import fs from "fs"
-import { day15Part01 } from "../src/day15"
+import { day15Part01, day15Part02 } from "../src/day15"
 
 const SAMPLE_INPUT = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
@@ -16,5 +16,15 @@ describe("Day 15", () => {
   test("Day 15 part 01", () => {
     const result = day15Part01(input)
     assert.strictEqual(result, 517015)
+  })
+
+  test("Day 15 part 02 sample input", () => {
+    const result = day15Part02(SAMPLE_INPUT)
+    assert.strictEqual(result, 145)
+  })
+  
+  test("Day 15 part 02", () => {
+    const result = day15Part02(input)
+    assert.strictEqual(result, 286104)
   })
 })
